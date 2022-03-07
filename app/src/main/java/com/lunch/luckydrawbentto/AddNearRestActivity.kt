802551ee178016853ba87cfb8aedfa951ad40040
beagleoasis <beagleoasis@naver.com>
2022-03-07 18:37:40 +0900

    20220307Auto stash before merge of "beagleoasisLocal" and "origin/kijongjo"

---
+++ app/src/main/java/com/lunch/luckydrawbentto/AddNearRestActivity.kt
@@ -0,0 +1,11 @@
+package com.lunch.luckydrawbentto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNearRestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_near_rest)
+    }
+}
